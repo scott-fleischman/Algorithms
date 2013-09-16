a3d4591be3fa393899f1ce1c35cffe3390cac47a
Scott Fleischman <scott.fleischman@logos.com>
2013-09-16 12:44:00 -0700

    Add basic linear search for index.

---
+++ src/Algorithms.Sort/LinearSearch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Sort
+{
+	public static class LinearSearch
+	{
+		// Ex 2.1-3, p.22
+		public static int? FindIndex(int value, IEnumerable<int> values)
+		{
+			int index = 0;
+			foreach (var currentValue in values)
+			{
+				if (currentValue == value)
+					return index;
+				index++;
+			}
+			return null;
+		}
+	}
+}
---
+++ tests/Algorithms.Sort.Tests/LinearSearchTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Algorithms.Sort.Tests
+{
+	[TestFixture]
+	public class LinearSearchTests
+	{
+		[TestCase(4, new[] { 1, 2, 3, 4 }, 3)]
+		[TestCase(5, new[] { 1, 2, 3, 4 }, null)]
+		[TestCase(0, new[] { 1, 2, 3, 4 }, null)]
+		[TestCase(1, new[] { 1, 2, 3, 4, 1 }, 0)]
+		public void TestNull(int value, int[] values, int? expectedIndex)
+		{
+			int? result = LinearSearch.FindIndex(value, values);
+			Assert.That(result, Is.EqualTo(expectedIndex));
+		}
+	}
+}
