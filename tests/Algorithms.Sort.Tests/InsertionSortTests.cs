39889c40aeac39e5780f16cbcc2ad53c9465428f
Scott Fleischman <scott.fleischman@logos.com>
2013-09-16 12:19:35 -0700

    Add extra test case

--- tests/Algorithms.Sort.Tests/InsertionSortTests.cs
+++ tests/Algorithms.Sort.Tests/InsertionSortTests.cs
@@ -5,11 +5,11 @@ namespace Algorithms.Sort.Tests
 {
 	public class InsertionSortTests
 	{
-		[Test]
-		public void TestSortInPlace()
+		[TestCase(new[] { 5, 2, 4, 6, 1, 3 }, new[] { 1, 2, 3, 4, 5, 6 })]
+		[TestCase(new[] { 31, 41, 59, 26, 41, 58 }, new[] { 26, 31, 41, 41, 58, 59 })]
+		public void TestSortInPlace(int[] elements, int[] expected)
 		{
-			var expected = new[] { 1, 2, 3, 4, 5, 6 };
-			var list = new List<int> { 5, 2, 4, 6, 1, 3 };
+			var list = new List<int>(elements);
 			InsertionSort.SortInPlace(list);
 			Assert.That(list, Is.EqualTo(expected));
 		}
