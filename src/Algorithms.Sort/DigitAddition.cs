890dd5972b1efea3ef274d2202a8e5a5cc584a28
Scott Fleischman <scott.fleischman@logos.com>
2013-09-17 09:22:28 -0700

    Add addition of sequence of integers for a given base.

---
+++ src/Algorithms.Sort/DigitAddition.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Sort
+{
+	public static class DigitAddition
+	{
+		public static IEnumerable<int> Add(IEnumerable<int> leftDigits, IEnumerable<int> rightDigits, int radix = 2)
+		{
+			using (var leftEnumerator = leftDigits.GetEnumerator())
+			using (var rightEnumerator = rightDigits.GetEnumerator())
+			{
+				int carry = 0;
+
+				while (true)
+				{
+					bool leftHasNext = leftEnumerator.MoveNext();
+					bool rightHasNext = rightEnumerator.MoveNext();
+
+					if (!leftHasNext && !rightHasNext)
+						break;
+
+					int leftDigit = leftHasNext ? leftEnumerator.Current : 0;
+					int rightDigit = rightHasNext ? rightEnumerator.Current : 0;
+
+					if (leftDigit < 0 || leftDigit >= radix)
+						throw new ArgumentException(string.Format("Invalid left digit: {0}", leftDigit));
+					if (rightDigit < 0 || rightDigit >= radix)
+						throw new ArgumentException(string.Format("Invalid right digit: {0}", rightDigit));
+
+					int newDigit = leftDigit + rightDigit + carry;
+					if (newDigit >= radix)
+					{
+						int total = newDigit;
+						carry = total / radix;
+						newDigit = total % radix;
+					}
+					else
+					{
+						carry = 0;
+					}
+
+					yield return newDigit;
+				}
+
+				if (carry != 0)
+					yield return carry;
+			}
+		}
+	}
+}
---
+++ tests/Algorithms.Sort.Tests/DigitAdditionTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Algorithms.Sort.Tests
+{
+	[TestFixture]
+	public class DigitAdditionTests
+	{
+		[TestCase("0", "0", "0")]
+		[TestCase("0", "1", "1")]
+		[TestCase("1", "0", "1")]
+		[TestCase("1", "1", "10")]
+		[TestCase("10", "1", "11")]
+		[TestCase("10", "10", "100")]
+		[TestCase("11", "1", "100")]
+		[TestCase("11", "10", "101")]
+		[TestCase("11", "11", "110")]
+		[TestCase("1111", "1", "10000")]
+		public static void Base2(string left, string right, string expected)
+		{
+			Func<string, IEnumerable<int>> toValues = x => x.AsEnumerable().Reverse().Select(c => c - '0');
+			Assert.That(DigitAddition.Add(toValues(left), toValues(right)), Is.EqualTo(toValues(expected)));
+		}
+
+		[TestCase("0", "0", "0")]
+		[TestCase("0", "1", "1")]
+		[TestCase("1", "1", "2")]
+		[TestCase("2", "1", "3")]
+		[TestCase("2", "2", "4")]
+		[TestCase("8", "1", "9")]
+		[TestCase("8", "2", "10")]
+		[TestCase("999", "1", "1000")]
+		public static void Base10(string left, string right, string expected)
+		{
+			Func<string, IEnumerable<int>> toValues = x => x.AsEnumerable().Reverse().Select(c => c - '0');
+			Assert.That(DigitAddition.Add(toValues(left), toValues(right), 10), Is.EqualTo(toValues(expected)));
+		}
+	}
+}
