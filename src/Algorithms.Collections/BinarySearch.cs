4a60eb9a85ff3ccd23c4b684619540d0451ce3ff
Scott Fleischman <scott.fleischman@logos.com>
2013-10-01 09:34:27 -0700

    Add binary search.

---
+++ src/Algorithms.Collections/BinarySearch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Collections
+{
+	public static class BinarySearch
+	{
+		// Ex 2.3-5
+		public static int? FindIndex<T>(IList<T> list, T value, IComparer<T> comparer)
+		{
+			int start = 0;
+			int end = list.Count - 1;
+
+			while (start <= end)
+			{
+				int middle = start + (end - start) / 2;
+				int result = comparer.Compare(list[middle], value);
+
+				if (result < 0)
+					start = middle + 1;
+				else if (result > 0)
+					end = middle - 1;
+				else
+					return middle;
+			}
+
+			return null;
+		}
+	}
+}
---
+++ tests/Algorithms.Collections.Tests/BinarySearchTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Algorithms.Collections.Tests
+{
+	[TestFixture]
+	public class BinarySearchTests
+	{
+		[TestCase("", 1, null)]
+		[TestCase("1", 1, 0)]
+		[TestCase("1", 2, null)]
+		[TestCase("1, 2", 1, 0)]
+		[TestCase("1, 2", 2, 1)]
+		[TestCase("1, 2, 3", 3, 2)]
+		[TestCase("1, 10, 100, 1000", 100, 2)]
+		[TestCase("-1, 0, 1", 0, 1)]
+		[TestCase("-2147483648, 0, 2147483647", -2147483648, 0)]
+		public void Test(string valuesString, int value, int? expectedIndex)
+		{
+			List<int> values = valuesString.Split(',')
+				.Where(x => !string.IsNullOrWhiteSpace(x))
+				.Select(x => int.Parse(x.Trim(), CultureInfo.InvariantCulture))
+				.ToList();
+			int? result = BinarySearch.FindIndex(values, value, Comparer<int>.Default);
+			Assert.That(result, Is.EqualTo(expectedIndex));
+		}
+	}
+}
