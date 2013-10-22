f915e2726e6f06727ca28b19e1cbdf12762dc6c3
Scott Fleischman <scott.fleischman@logos.com>
2013-10-22 09:47:20 -0700

    Finish maximum sublist with tests.

--- src/Algorithms.Collections/MaximumSublist.cs
+++ src/Algorithms.Collections/MaximumSublist.cs
@@ -5,23 +5,28 @@ namespace Algorithms.Collections
 {
 	public static class MaximumSublist
 	{
+		public static SublistSum<T> GetMaximumSublist<T>(IList<T> list, Func<T, T, T> add)
+		{
+			return GetMaximumSublist(list, add, Comparer<T>.Default);
+		}
+
 		// Ch 4.1, p. 72
-		public static SublistResult<T> GetMaximumSublist<T>(IList<T> list, Func<T, T, T> add, IComparer<T> comparer)
+		public static SublistSum<T> GetMaximumSublist<T>(IList<T> list, Func<T, T, T> add, IComparer<T> comparer)
 		{
 			if (list.Count == 0)
 				throw new ArgumentException("list has no elements", "list");
 			return GetMaximumSublist(list, 0, list.Count - 1, add, comparer);
 		}
 
-		public static SublistResult<T> GetMaximumSublist<T>(IList<T> list, int startIndex, int endIndex, Func<T, T, T> add, IComparer<T> comparer)
+		public static SublistSum<T> GetMaximumSublist<T>(IList<T> list, int startIndex, int endIndex, Func<T, T, T> add, IComparer<T> comparer)
 		{
 			if (startIndex == endIndex)
-				return new SublistResult<T>(startIndex, endIndex, list[startIndex]);
+				return SublistSum.Create(startIndex, endIndex, list[startIndex]);
 
 			int middleIndex = (startIndex + endIndex) / 2;
-			SublistResult<T> left = GetMaximumSublist(list, startIndex, middleIndex, add, comparer);
-			SublistResult<T> right = GetMaximumSublist(list, middleIndex + 1, endIndex, add, comparer);
-			SublistResult<T> cross = GetMaximumCrossingSublist(list, startIndex, middleIndex, endIndex, add, comparer);
+			SublistSum<T> left = GetMaximumSublist(list, startIndex, middleIndex, add, comparer);
+			SublistSum<T> right = GetMaximumSublist(list, middleIndex + 1, endIndex, add, comparer);
+			SublistSum<T> cross = GetMaximumCrossingSublist(list, startIndex, middleIndex, endIndex, add, comparer);
 
 			if (comparer.Compare(left.Sum, right.Sum) >= 0 && comparer.Compare(left.Sum, cross.Sum) >= 0)
 				return left;
@@ -31,15 +36,51 @@ namespace Algorithms.Collections
 				return cross;
 		}
 
-		private static SublistResult<T> GetMaximumCrossingSublist<T>(IList<T> list, int startIndex, int middleIndex, int endIndex, Func<T, T, T> add, IComparer<T> comparer)
+		private static SublistSum<T> GetMaximumCrossingSublist<T>(IList<T> list, int startIndex, int middleIndex, int endIndex, Func<T, T, T> add, IComparer<T> comparer)
 		{
-			throw new NotImplementedException();
+			int leftIndex = middleIndex;
+			T leftSum = list[middleIndex];
+
+			T sum = leftSum;
+			for (int index = leftIndex - 1; index >= startIndex; index--)
+			{
+				sum = add(sum, list[index]);
+				if (comparer.Compare(sum, leftSum) > 0)
+				{
+					leftIndex = index;
+					leftSum = sum;
+				}
+			}
+
+			int rightIndex = middleIndex + 1;
+			T rightSum = list[rightIndex];
+
+			sum = rightSum;
+			for (int index = rightIndex + 1; index <= endIndex; index++)
+			{
+				sum = add(sum, list[index]);
+				if (comparer.Compare(sum, rightSum) > 0)
+				{
+					rightIndex = index;
+					rightSum = sum;
+				}
+			}
+
+			return SublistSum.Create(leftIndex, rightIndex, add(leftSum, rightSum));
 		}
 	}
 
-	public class SublistResult<T>
+	public static class SublistSum
 	{
-		public SublistResult(int startIndex, int endIndex, T sum)
+		public static SublistSum<T> Create<T>(int startIndex, int endIndex, T sum)
+		{
+			return new SublistSum<T>(startIndex, endIndex, sum);
+		}
+	}
+
+	public class SublistSum<T>
+	{
+		public SublistSum(int startIndex, int endIndex, T sum)
 		{
 			m_startIndex = startIndex;
 			m_endIndex = endIndex;
---
+++ tests/Algorithms.Collections.Tests/MaximumSublistTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Algorithms.Collections.Tests
+{
+	[TestFixture]
+	public class MaximumSublistTests
+	{
+		[TestCaseSource("GetTestCases_Int32")]
+		public void GetMaximumSublist_Int32(TestCase<int> testCase)
+		{
+			var result = MaximumSublist.GetMaximumSublist(testCase.Items, (x, y) => x + y);
+			Assert.AreEqual(testCase.Expected.StartIndex, result.StartIndex);
+			Assert.AreEqual(testCase.Expected.EndIndex, result.EndIndex);
+			Assert.AreEqual(testCase.Expected.Sum, result.Sum);
+		}
+
+		public IEnumerable<TestCase<int>> GetTestCases_Int32()
+		{
+			return new[]
+				{
+					TestCase.Create(new[] { 1 }, SublistSum.Create(0, 0, 1)),
+					TestCase.Create(new[] { 1, 2 }, SublistSum.Create(0, 1, 3)),
+					TestCase.Create(new[] { 1, -2 }, SublistSum.Create(0, 0, 1)),
+					TestCase.Create(new[] { 1, -4, 3, -4 }, SublistSum.Create(2, 2, 3)),
+					TestCase.Create(new[] { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 },
+						SublistSum.Create(7, 10, 18 + 20 + -7 + 12)),
+				};
+		}
+
+		public static class TestCase
+		{
+			public static TestCase<T> Create<T>(IEnumerable<T> items, SublistSum<T> expected)
+			{
+				return new TestCase<T>(items, expected);
+			}
+		}
+
+		public class TestCase<T>
+		{
+			public TestCase(IEnumerable<T> items, SublistSum<T> expected)
+			{
+				m_items = items.ToList();
+				m_expected = expected;
+			}
+
+			public IList<T> Items
+			{
+				get { return m_items; }
+			}
+
+			public SublistSum<T> Expected
+			{
+				get { return m_expected; }
+			}
+
+			readonly IList<T> m_items;
+			readonly SublistSum<T> m_expected;
+		}
+	}
+}
